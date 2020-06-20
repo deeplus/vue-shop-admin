422badd8da494801c63339048b5a7281627f894e
deeplus <deeplus@163.com>
2020-06-20 15:00:55 +0800

    完成login页面的路由分配

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '../components/Login'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
-    routes: []
+    routes: [
+        {
+            path: '/',
+            redirect: '/login'
+        },
+        {
+            path: '/login',
+            component: Login
+        }
+    ]
 })
